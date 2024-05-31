54bbda3c3ff96a66bc0af48ae69b0984e6a660e3
Steven Grundner <stevengrundner@Stevens-MacBook-Pro.local>
2024-05-31 12:05:39 -0600

    boxes in order

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ document.getElementById('add').addEventListener('click', () => {
     let table = document.getElementById('list');
     let row = table.insertRow(1);
     row.setAttribute('id', `item-${id}`);
-    row.insertCell(0).innerHTML = document.getElementById('new-ski-run').value;
-    row.insertCell(1).innerHTML = `${createdDate.getFullYear()}-${createdDate.getMonth() + 1}-${createdDate.getDate()}`;
-    row.insertCell(2).innerHTML = document.getElementById ('new-opening-date').value;
+    row.insertCell(0).innerHTML = document.getElementById('new-area').value;
+    row.insertCell(1).innerHTML = document.getElementById ('new-opening-date').value;
+    row.insertCell(2).innerHTML = document.getElementById('new-ski-run').value;
     row.insertCell(3).innerHTML = document.getElementById ('new-ski-date').value;
     let actions = row.insertCell(4);
     actions.appendChild(createDeleteButton(id++)); 
