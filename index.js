67553e679044944eaa32881312a3b057626bec42
Steven Grundner <stevengrundner@Stevens-MacBook-Pro.local>
2024-05-31 11:10:10 -0600

    updated names

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ document.getElementById('add').addEventListener('click', () => {
     let table = document.getElementById('list');
     let row = table.insertRow(1);
     row.setAttribute('id', `item-${id}`);
-    row.insertCell(0).innerHTML = document.getElementById('new-task').value;
+    row.insertCell(0).innerHTML = document.getElementById('new-ski-run').value;
     row.insertCell(1).innerHTML = `${createdDate.getFullYear()}-${createdDate.getMonth() + 1}-${createdDate.getDate()}`;
-    row.insertCell(2).innerHTML = document.getElementById ('new-start-date').value;
-    row.insertCell(3).innerHTML = document.getElementById ('new-end-date').value;
+    row.insertCell(2).innerHTML = document.getElementById ('new-opening-date').value;
+    row.insertCell(3).innerHTML = document.getElementById ('new-ski-date').value;
     let actions = row.insertCell(4);
     actions.appendChild(createDeleteButton(id++)); 
-    document.getElementById('new-task').value = '';
+    document.getElementById('new-ski-run').value = '';
 });
 
 function createDeleteButton(id){
